4d5568411f87825f1374e2d9d33e24a1de061651
André Luiz Morita <andreluizmorita@gmail.com>
2019-04-15 14:17:55 -0300

    Fix autosave

--- src/main/java/com/cobranca/controller/TituloController.java
+++ src/main/java/com/cobranca/controller/TituloController.java
@@ -23,66 +23,66 @@ import com.cobranca.service.TituloService;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-	
+
 	private static final String CADASTRO_VIEW = "CadastroTitulo";
-	
+
 	@Autowired
 	private TituloService tituloService;
-	
+
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);	
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject(new Titulo());
 		return mv;
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
 	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
-		if(errors.hasErrors()) {
+		if (errors.hasErrors()) {
 			return CADASTRO_VIEW;
 		}
-		
+
 		try {
 			tituloService.salvar(titulo);
 			attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
-			
-			return "redirect:/titulos/novo"; 
-		} catch(IllegalArgumentException e) {
+
+			return "redirect:/titulos/novo";
+		} catch (IllegalArgumentException e) {
 			System.out.println("Data inválida");
 			errors.rejectValue("dataVencimento", null, e.getMessage());
 			return CADASTRO_VIEW;
 		}
 
 	}
-	
+
 	@RequestMapping
 	public ModelAndView pesquisar(@ModelAttribute("filtro") TituloFilter filtro) {
 		List<Titulo> todosTitulos = tituloService.filtrar(filtro);
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos", todosTitulos);
-		
+
 		return mv;
 	}
-	
+
 	@RequestMapping("{codigo}")
 	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
 		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject("titulo", titulo);
 		return mv;
 	}
-	
+
 	@RequestMapping(value = "{codigo}", method = RequestMethod.DELETE)
-	public String excluir(@PathVariable Long codigo, Errors errors, RedirectAttributes attr) {		
+	public String excluir(@PathVariable Long codigo, Errors errors, RedirectAttributes attr) {
 		tituloService.excluir(codigo);
 		attr.addFlashAttribute("mensagem", "Título excluído com sucesso!");
 		return "redirect:/titulos";
 	}
-	
+
 	@RequestMapping(value = "/{codigo}/receber", method = RequestMethod.PUT)
 	public @ResponseBody String receber(@PathVariable Long codigo) {
 		return tituloService.receber(codigo);
 	}
-	
+
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulo() {
 		return Arrays.asList(StatusTitulo.values());
